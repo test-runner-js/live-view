e5d809b0a8b56835c54b7e3ae4a6f5565ba39447
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-11-21 21:48:35 +0000

    initial

---
+++ index.mjs
@@ -0,0 +1,124 @@
+/**
+ * Custom view API.
+ */
+class TreeView {
+  constructor (options) {
+    this.options = options
+    this.fails = []
+  }
+
+  log (...args) {
+    const ansi = require('ansi-escape-sequences')
+    if (this.prevLineCount) {
+      process.stdout.write(ansi.cursor.previousLine(this.prevLineCount))
+    }
+    console.log(ansi.format(args.join(' ')))
+  }
+
+  /**
+   * Runner start.
+   * @param {number}
+   */
+  start (count) {
+  }
+
+  /**
+   * Test start
+   * @param {Tom}
+   */
+  testStart (test) {
+    this.printTree()
+  }
+
+  /**
+   * Test passed.
+   * @param {Tom}
+   * @param {*}
+   */
+  testPass (test, result) {
+    this.printTree()
+  }
+
+  /**
+   * Test passed.
+   * @param {Tom}
+   * @param {*}
+   */
+  testFail (test, err) {
+    this.printTree()
+    this.fails.push(test)
+  }
+
+  /**
+   * Test skipped.
+   * @param {Tom}
+   */
+  testSkip (test) {
+    this.printTree()
+  }
+
+  /**
+   * @params {object} stats
+   * @params {object} stats.fail
+   * @params {object} stats.pass
+   * @params {object} stats.skip
+   * @params {object} stats.start
+   * @params {object} stats.end
+   */
+  end (stats) {
+    this.prevLineCount = 0
+    const failColour = stats.fail > 0 ? 'red' : 'white'
+    const passColour = stats.pass > 0 ? 'green' : 'white'
+    const skipColour = stats.skip > 0 ? 'grey' : 'white'
+    this.log(`\n[white]{Completed in ${stats.timeElapsed()}ms. Pass: [${passColour}]{${stats.pass}}, fail: [${failColour}]{${stats.fail}}, skip: [${skipColour}]{${stats.skip}}.}\n`)
+
+    const fails = []
+    for (const test of this.fails) {
+      const err = test.result
+      const indent = ' '.repeat(test.level())
+      const parent = test.parent ? test.parent.name : ''
+      fails.push(`${indent}[red]{⨯} [magenta]{${parent}} ${test.name}`)
+      const lines = err.stack.split('\n').map(line => {
+        const indent = ' '.repeat(test.level() + 2)
+        return indent + line
+      })
+      fails.push(`\n${lines.join('\n').trimEnd()}\n`)
+    }
+    if (fails.length) {
+      this.log(fails.join('\n'))
+    }
+  }
+
+  printTree () {
+    const ansi = require('ansi-escape-sequences')
+    const groups = Array.from(this.runner.tom).filter(t => t.state === 'ignored')
+    const lines = []
+    for (const group of groups) {
+      const line = [ansi.erase.inLine(2)]
+      const indent = ' '.repeat(group.level())
+      line.push(`${indent}- [magenta]{${group.name}} `)
+      for (const test of group.children) {
+        if (test.state === 'pass') {
+          line.push('[green]{✓}')
+        } else if (test.state === 'fail') {
+          line.push('[red]{⨯}')
+        } else if (test.state === 'in-progress') {
+          line.push('[cyan]{•}')
+        } else if (test.state === 'skipped') {
+          line.push('[grey]{-}')
+        }
+      }
+      lines.push(line.join(''))
+    }
+    this.log(lines.join('\n'))
+    this.prevLineCount = lines.length
+  }
+
+  /**
+   * Option definitions.
+   * @returns {OptionDefinition[]}
+   */
+  static optionDefinitions () {}
+}
+
+export default TreeView
